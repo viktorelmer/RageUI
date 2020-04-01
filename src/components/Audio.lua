0b8ff217b9a41ada392a3635c2049a5a0fe3b583
Dylan Malandain <lucenzo.76@gmail.com>
2020-04-01 20:59:37 +0200

    Fix Audio ID issue with panel

--- src/components/Audio.lua
+++ src/components/Audio.lua
@@ -14,24 +14,19 @@
 ---@param IsLooped boolean
 ---@return nil
 ---@public
-function RageUI.PlaySound(Library, Sound, IsLooped,  Audio)
-
+function RageUI.PlaySound(Library, Sound, IsLooped)
+    local audioId
     if not IsLooped then
         PlaySoundFrontend(-1, Sound, Library, true)
     else
-        if not Audio.Id then
+        if not audioId then
             Citizen.CreateThread(function()
-                Audio.Id = GetSoundId()
-
-                PlaySoundFrontend(Audio.Id, Sound, Library, true)
-
+                audioId = GetSoundId()
+                PlaySoundFrontend(audioId, Sound, Library, true)
                 Citizen.Wait(0.01)
-
-                StopSound(Audio.Id)
-
-                ReleaseSoundId(Audio.Id)
-
-                Audio.Id = nil
+                StopSound(audioId)
+                ReleaseSoundId(audioId)
+                audioId = nil
             end)
         end
     end
