35d6d8a8321a89ab19770f457ba99cd84b87f1e3
Dylan Malandain <lucenzo.76@gmail.com>
2020-04-01 21:02:52 +0200

    Implemented news tests menu with complete feature

--- tests/rmenu/basic.lua
+++ tests/rmenu/basic.lua
@@ -12,6 +12,7 @@ RMenu:Get('showcase', 'main').Closed = function()
 end;
 
 RMenu.Add('showcase', 'submenu', RageUI.CreateSubMenu(RMenu:Get('showcase', 'main'), "RageUI", "~b~RAGEUI SHOWCASE", nil, nil, "root_cause", "shopui_title_dynasty8"))
+RMenu:Get('showcase', 'submenu').EnableMouse = true
 
 ---@type table
 local foods = {
@@ -27,122 +28,102 @@ local index = {
     ketchup = false;
     dish = 1;
     quantity = 0;
+    panel = {
+        percentage = 0.5
+    }
 }
 
 ---@type string
 local description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
 
-local items = 10;
+RageUI.CreateWhile(1.0, RMenu:Get('showcase', 'main'), 51, function()
 
-RageUI.CreateWhile(1.0, true, function()
-
-    if IsControlJustPressed(1, 51) then
-        RageUI.Visible(RMenu:Get('showcase', 'main'), not RageUI.Visible(RMenu:Get('showcase', 'main')))
-    end
-
-    if RageUI.Visible(RMenu:Get('showcase', 'main')) then
-        RageUI.DrawContent({ header = true, glare = true, instructionalButton = true }, function()
-            ---Items
-            RageUI.Checkbox("Add ketchup ?", "Do you wish to add ketchup ?", index.ketchup, { Style = RageUI.CheckboxStyle.Tick }, function(Hovered, Selected, Active, Checked)
-                if (Hovered) then
-
-                end
-                if (Active) then
-
-                end
-                if (Selected) then
-                    RageUI.Text({
-                        message = string.format("~r~Ketchup status: ~b~%s", tostring(index.ketchup))
-                    })
-                end
-                index.ketchup = Checked;
-            end)
-
-            RageUI.List("Food", foods, index.dish, "Select the type of food you want to eat.", {}, true, function(Hovered, Active, Selected, Index)
-                if (Hovered) then
-
-                end
-                if (Active) then
-                    RageUI.Text({
-                        message = string.format("Preparing ~b~%s~w~...", foods[index.dish])
-                    })
-                end
-                if (Selected) then
-
-                end
-                index.dish = Index;
-            end)
-
-            RageUI.Slider("Quantity", index.quantity, 10, "Select the amount of food you want to eat.", false, { }, true, function(Hovered, Selected, Active, Index)
-                if (Hovered) then
-
-                end
-                if (Selected) then
-                    RageUI.Text({
-                        message = string.format("Preparing ~r~%s ~b~%s(s)~w~...", index.quantity, foods[index.dish])
-                    })
-                end
-                if (Active) then
-
-                end
-                index.quantity = Index;
-            end)
-
-            RageUI.Button("Cook !", "Cook the dish with the appropriate ingredients and ketchup.", { RightBadge = RageUI.BadgeStyle.Tick }, true, function(Hovered, Active, Selected)
-                if (Hovered) then
-
-                end
-                if (Active) then
-
-                end
-                if (Selected) then
-                    local string = string.format("You have ordered ~r~%s ~b~%s(s)~w~ ~r~with~w~ ketchup.", index.quantity, foods[index.dish])
-                    if not (index.ketchup) then
-                        string = string.format("You have ordered ~r~%s ~b~%s(s)~w~ ~r~without~w~ ketchup.", index.quantity, foods[index.dish])
-                    end
-                    RageUI.Text({
-                        message = string;
-                    })
-                end
-            end)
-
-            RageUI.Button("Another Menu", "Sample description that takes more than one line. Moreso, it takes way more than two lines since it's so long. Wow, check out this length !", { RightLabel = "→→→" }, true, function(Hovered, Active, Selected)
-                if (Hovered) then
-
-                end
-                if (Active) then
-
-                end
-                if (Selected) then
-
-                end
-            end, RMenu:Get('showcase', 'submenu'))
-
-        end, function()
-            ---Panels
-        end)
-    end
-
-    if RageUI.Visible(RMenu:Get('showcase', 'submenu')) then
-        RageUI.DrawContent({ header = true, glare = true, instructionalButton = true }, function()
-            ---Items
-
-            for i = 1, items do
-                RageUI.Button(string.format("Items - %s", i), "Sample description that takes more than one line. Moreso, it takes way more than two lines since it's so long. Wow, check out this length !", { RightLabel = "→→→" }, true, function(Hovered, Active, Selected)
-
-                end)
+    RageUI.IsVisible(RMenu:Get('showcase', 'main'), true, true, true, function()
+        --- Items
+        RageUI.Checkbox("Add ketchup ?", "Do you wish to add ketchup ?", index.ketchup, { Style = RageUI.CheckboxStyle.Tick }, function(Hovered, Selected, Active, Checked)
+            if (Selected) then
+                RageUI.Text({
+                    message = string.format("~r~Ketchup status: ~b~%s", tostring(index.ketchup))
+                })
             end
-
-
-        end, function()
-            ---Panels
+            index.ketchup = Checked;
         end)
-    end
 
-end, 1)
+        RageUI.List("Food", foods, index.dish, "Select the type of food you want to eat.", {}, true, function(Hovered, Active, Selected, Index)
+            if (Active) then
+                RageUI.Text({
+                    message = string.format("Preparing ~b~%s~w~...", foods[index.dish])
+                })
+            end
+            index.dish = Index;
+        end)
 
-RegisterCommand('delete-basic-rmenu', function()
-    --- You can remove unnecessary menus to optimize the use of RAM in your scripts.
-    RMenu:Delete('showcase', 'main')
-    RMenu:Delete('showcase', 'submenu')
-end, false)
+        RageUI.Slider("Quantity", index.quantity, 10, "Select the amount of food you want to eat.", false, { }, true, function(Hovered, Selected, Active, Index)
+            if (Selected) then
+                RageUI.Text({
+                    message = string.format("Preparing ~r~%s ~b~%s(s)~w~...", index.quantity, foods[index.dish])
+                })
+            end
+            index.quantity = Index;
+        end)
+
+        RageUI.Button("Cook !", "Cook the dish with the appropriate ingredients and ketchup.", { RightBadge = RageUI.BadgeStyle.Tick }, true, function(Hovered, Active, Selected)
+            if (Selected) then
+                local string = string.format("You have ordered ~r~%s ~b~%s(s)~w~ ~r~with~w~ ketchup.", index.quantity, foods[index.dish])
+                if not (index.ketchup) then
+                    string = string.format("You have ordered ~r~%s ~b~%s(s)~w~ ~r~without~w~ ketchup.", index.quantity, foods[index.dish])
+                end
+                RageUI.Text({
+                    message = string;
+                })
+            end
+        end)
+
+        RageUI.Button("Another Menu", description, { RightLabel = "→→→" }, true, function(Hovered, Active, Selected)
+            if (Selected) then
+
+            end
+        end, RMenu:Get('showcase', 'submenu'))
+
+    end, function()
+        ---Panels
+
+    end)
+
+    RageUI.IsVisible(RMenu:Get('showcase', 'submenu'), false, false, false, function()
+        --- Items
+        for i = 1, 100 do
+            RageUI.Button(string.format("Items - %s", i), description, {}, true, function(Hovered, Active, Selected)
+                if (Selected) then
+
+                end
+            end)
+        end
+    end, function()
+        ---Panels
+        RageUI.GridPanelHorizontal(1, 0.5, "Ouverts", "Plissés", function(Hovered, Active, X)
+            -- TODO Implemented working
+        end)
+        RageUI.GridPanel(2, 0.5, 0.5, "Haut", "Bas", "Intérieur", "Extérieur", function(Hovered, Active, X, Y)
+            -- TODO Implemented working
+        end)
+        RageUI.GridPanelVertical(4, 0.5, "Ouverts", "Plissés", function(Hovered, Active, Y)
+            -- TODO Implemented working
+        end)
+        RageUI.ColourPanel(5, "Couleur principale", RageUI.PanelColour.HairCut, 1, 5, function(Hovered, Active, MinimumIndex, CurrentIndex)
+            -- TODO Implemented working
+        end)
+        RageUI.PercentagePanel(7, index.panel.percentage, "Percentage", nil, nil, function(Hovered, Active, Percent)
+            if (Active) then
+
+            end
+            index.panel.percentage = Percent
+        end)
+
+        RageUI.StatisticPanel(7, 0.9, "Weapon MK2")
+        RageUI.StatisticPanel(7, 0.6, "Weapon")
+    end)
+
+end)
+
+
