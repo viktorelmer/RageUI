3355a02cd7ba86d3c5e1b0be144d1f0b2d144efa
SpikeScarlatti <47086150+SpikeScarlatti@users.noreply.github.com>
2020-04-02 16:12:14 +0200

    Add files via upload

--- src/menu/items/UIButton.lua
+++ src/menu/items/UIButton.lua
@@ -8,36 +8,6 @@ local SettingsButton = {
     SelectedSprite = { Dictionary = "commonmenu", Texture = "gradient_nav", Y = 0, Width = 431, Height = 38 },
 }
 
-function DumpTable()
-    if nb == nil then
-        nb = 0
-    end
-
-    if type(table) == 'table' then
-        local s = ''
-        for i = 1, nb + 1, 1 do
-            s = s .. "    "
-        end
-
-        s = '{\n'
-        for k,v in pairs(table) do
-            if type(k) ~= 'number' then k = '"'..k..'"' end
-            for i = 1, nb, 1 do
-                s = s .. "    "
-            end
-            s = s .. '['..k..'] = ' .. DumpTable(v, nb + 1) .. ',\n'
-        end
-
-        for i = 1, nb, 1 do
-            s = s .. "    "
-        end
-
-        return s .. '}'
-    else
-        return tostring(table)
-    end
-end
-
 ---Button
 ---@param Label string
 ---@param Description string
