2f3ca0ed0294e9f60dd32eae3250e53d697d7dc4
Dylan Malandain <lucenzo.76@gmail.com>
2020-05-26 16:41:47 +0200

    Update UISeparator.lua

--- src/client/menu/items/UISeparator.lua
+++ src/client/menu/items/UISeparator.lua
@@ -4,14 +4,13 @@ local SettingsButton = {
     Text = { X = 8, Y = 3, Scale = 0.33 },
 }
 
-function RageUI.Separator(Label)
-    local CurrentMenu = RageUI.CurrentMenu
+local CurrentMenu = RageUI.CurrentMenu
     if CurrentMenu ~= nil then
         if CurrentMenu() then
             local Option = RageUI.Options + 1
             if CurrentMenu.Pagination.Minimum <= Option and CurrentMenu.Pagination.Maximum >= Option then
                 if (Label ~= nil) then
-                    RenderText(Label, CurrentMenu.X + SettingsButton.Text.X + 250.0, CurrentMenu.Y + SettingsButton.Text.Y + CurrentMenu.SubtitleHeight + RageUI.ItemOffset, 0, SettingsButton.Text.Scale, 245, 245, 245, 255,1)
+                    RenderText(Label, CurrentMenu.X + SettingsButton.Text.X + (CurrentMenu.WidthOffset * 2.5 ~= 0 and CurrentMenu.WidthOffset * 2.5 or 200), CurrentMenu.Y + SettingsButton.Text.Y + CurrentMenu.SubtitleHeight + RageUI.ItemOffset, 0, SettingsButton.Text.Scale, 245, 245, 245, 255, 1)
                 end
                 RageUI.ItemOffset = RageUI.ItemOffset + SettingsButton.Rectangle.Height
                 if (CurrentMenu.Index == Option) then
@@ -28,4 +27,3 @@ function RageUI.Separator(Label)
             RageUI.Options = RageUI.Options + 1
         end
     end
-end
