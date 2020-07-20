d68c67a43f82e3f5e30c417ee7c052d9454bb3df
iTexZoz <contact@dylan-malandain.io>
2020-07-20 15:45:52 +0200

    Update project structure.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -19,15 +19,15 @@ name 'RageUI';
 description 'RageUI Is Advanced UI Libs in LUA for make beautiful interface like RockStar GAME.'
 
 client_scripts {
-    "src/client/RMenu.lua",
-    "src/client/menu/RageUI.lua",
-    "src/client/menu/Menu.lua",
-    "src/client/menu/MenuController.lua",
-    "src/client/components/*.lua",
-    "src/client/menu/elements/*.lua",
-    "src/client/menu/items/*.lua",
-    "src/client/menu/panels/*.lua",
-    "src/client/menu/windows/*.lua",
+    "RMenu.lua",
+    "menu/RageUI.lua",
+    "menu/Menu.lua",
+    "menu/MenuController.lua",
+    "components/*.lua",
+    "menu/elements/*.lua",
+    "menu/items/*.lua",
+    "menu/panels/*.lua",
+    "menu/windows/*.lua",
 }
 
 client_scripts {
